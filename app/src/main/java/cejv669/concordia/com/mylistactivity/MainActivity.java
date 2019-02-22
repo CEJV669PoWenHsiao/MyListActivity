8a51eb32fdff3f671d3aaa5bf74be0855151f6af
Po-Wen Hsiao <p_hsiao@concordia.ca>
2019-02-21 21:42:00 -0500

    Initial commit

---
+++ app/src/main/java/cejv669/concordia/com/mylistactivity/MainActivity.java
@@ -0,0 +1,24 @@
+package cejv669.concordia.com.mylistactivity;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+public class MainActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] mApps = {"Skype", "WhatsApp", "Yahoo Messenger"};
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, mApps);
+
+        setListAdapter(adapter);
+
+    }
+
+
+}
